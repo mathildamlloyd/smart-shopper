63eed1b5406f560259386578cf3461603548b07d
Mathilda Lloyd <mathildamlloyd@gmail.com>
2014-09-13 13:23:58 -0700

    added search results page

--- webapp/app.js
+++ webapp/app.js
@@ -32,6 +32,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 app.get('/search', search.search);
+app.post('/search', search.postSearch);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- webapp/routes/search.js
+++ webapp/routes/search.js
@@ -7,3 +7,8 @@ exports.search = function(req, res){
   res.render('search', { title: 'Our Search Page' })
 };
 
+exports.postSearch = function(req, res){
+  var query = req.query;
+  var lookup = query['search-params'];
+  res.render('searchResult', { lookup: lookup });
+};
